a1ec45c92ee3b44a104ed4734716bd12943c169f
mariela <36265002+mariela27@users.noreply.github.com>
2018-02-13 21:26:54 -0300

    alert, prompt, ID
    Código reescrito

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var dato;
+	dato=prompt("");
+	alert("dato");
+
+}
+
