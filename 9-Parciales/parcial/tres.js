fe21a76b6c49f005534a2a36ebbe183cbeafc5ba
mariela <36265002+mariela27@users.noreply.github.com>
2018-02-15 00:50:03 -0300

    revisar

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,7 @@
 function Mostrar()
 {
+	  var largo = parseInt(document.getElementById("alrgo").value);
+    var ancho = parseInt(document.getElementById("ancho").value);
+    alert("Para rodear 3 veces el terreno se necesitan " + (((largo + ancho)*2)*3) + " metros de alambre");
 
 }
