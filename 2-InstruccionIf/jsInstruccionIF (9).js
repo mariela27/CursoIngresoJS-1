e07d156d5dac471ba2d50490855d92ad8201b46f
mariela <36265002+mariela27@users.noreply.github.com>
2018-02-15 12:24:50 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,30 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numero;
+	numero=(Math.random()*(10-1)+1);
+	numero=parsenInt("numero");
+	if(numero>=9)
+	{
+		alert("excelente")
+	}	
+	else if(nota >=6) 
+	{
+		alert("muy bien")	
+	}
+	else if(numero>=4)
+	{
+		alert("bien")
+	}
+	else if(numero>=2)
+	
+	{
+		alert("mal")
+	}	
+	else if(numero>=0)
+	{
+		alert("desastroso")
+	}
 	
 
 }//FIN DE LA FUNCIÓN
