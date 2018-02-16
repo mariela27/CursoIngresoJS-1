ce465a0d72ac4584f9bc8aad9011ea70d86b85e6
mariela <36265002+mariela27@users.noreply.github.com>
2018-02-16 11:56:26 -0300

    verificar el 6

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,10 +1,22 @@
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    //tomo la edad  
+    var mesDelAño 
+    mesDelAño=document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
+    //alert (mesDelAño);
+    switch(mesDelAño)
+    {
+        default: 
+        alert("Este mes tiene 30 o más días");
+        break;     
+        case "Febrero":
+            alert("Este mes no tiene más de 29 días.");
+            break;
+    
+         
+    }
+        
 	
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,9 +1,25 @@
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    //tomo la edad  
+    var mesDelAño;
+    mesDelAño= document.getElementById('mes').value;
 
-//alert (mesDelAño);
+    //alert (mesDelAño);
+    switch(mesDelAño)
+    {
+        case "Febrero":
+            alert("tiene 28 dias");
+            break;
+        case "Abril":
+        case "Junio":
+        case "Septiembre":
+        case "Noviembre":
+            alert("tiene 30 dias");
+            break;
+        default:
+            alert("tiene 31 dias");
+            break;
+    }
 	
 	
 
