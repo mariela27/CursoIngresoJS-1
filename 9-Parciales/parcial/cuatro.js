8c4a673924dd48ebf3b7abbaa2a51e8b54b32c51
mariela <36265002+mariela27@users.noreply.github.com>
2018-02-15 23:27:08 -0300

    revisar

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,30 @@
 function Mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+	numeroUno=prompt("Ingrese el primer numero: ", "");
+	numeroDos=prompt("ingrese el segundo numero:", "");
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	if(numeroUno == numeroDos)
+	{
+	   resultado = numeroUno * numeroDos;
+	}		
+	else
+	{
+	   if(numeroUno > numeroDos)
+	   {
+		resultado = numeroUno - numeroDos;
+	   }
+	   else
+	   {
+		resultado = numeroUno + numeroDos;
+	   }
+	}
+	document.write(resultado);
+
+
+
 
 }
